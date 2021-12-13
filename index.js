a03afa1d8b41e2a1b18ff1e295d502f70dd03c2d
Michael Stafford <michaelstafford808@gmail.com>
2021-12-13 11:28:45 -0800

    commit tree 8aa38f8f4fb4f63f66678ade3010bd430fe34321

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record) {
+  const result = record.find(record => record.result === "W")
+  return (result ? result.year : undefined);
+}
 
+
+// if (result.year === "W") {
+//   return true
+// } else {
+//   return undefined
+// }
